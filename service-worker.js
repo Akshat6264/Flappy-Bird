b14732ae79d10456d585b6b54dbd7590bc2e5ada
Akshat6264 <akshatkelotra@gmail.com>
2023-08-03 23:17:18 +0530

    offline working

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,30 @@
-const CACHE_NAME = 'offline-game-cache-v1';
-const urlsToCache = [
-  '/',
-  'styles.css',
-  'game.js',
-  'Images/flappybird.png',
-  'Images/toppipe.png',
-  'Images/bottompipe.png',
-];
+// const CACHE_NAME = 'offline-game-cache-v1';
+// const urlsToCache = [
+//   '/',
+//   'styles.css',
+//   'game.js',
+//   'Images/flappybird.png',
+//   'Images/toppipe.png',
+//   'Images/bottompipe.png',
+// ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(cache => {
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       })
+//   );
+// });
